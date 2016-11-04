a533837183f596dadf07f97c325d89d0c2f3b2ed
DESKTOP-4BRJN43\Xander <xdenduijn@gmail.com>
2016-11-04 17:28:21 +0100

    Update code

--- navigation_gps.py
+++ navigation_gps.py
@@ -26,13 +26,29 @@ for line in obs_file:
         break   
 
 #print header
-
+    
+#read all observation info
 for i in range(cnt,len(obs_file)):
+    epoch = 1
+    line = obs_file[i]
+    next_line = obs_file[i+1]
     # we assume Galileo sats are used
     # what if only Glonass sats are used??
-    nr_sats = obs_file[i][30:obs_file[i].find('G')]
+    nr_sats = int(line[30:line.find('G')])
+    print nr_sats
+    # get epoch
+    epoch = line[0:26]
+    print epoch
+    flag = line[28:29]
+    print flag
     if nr_sats > 12:
-        print 'more than one line is used'
+        sat_id = line[32:-1]+next_line[32:-1]
+        print sat_id
+        # read obervations for every epoch
+        for i in range(i+2,(i+2+nr_sats)):
+            print line[:-1]
+
+    # in case nr sats <= 12
     break
     
     
